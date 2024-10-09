f4c897a1272aa4a274ada4643df0bd3a238cd09f
Andrés Vargas <andres.pinneros@iepgroup.es>
2024-10-09 21:03:23 +0200

    estilos de carrusel agregados y funcionando

--- src/components/mainNewComponent.vue
+++ src/components/mainNewComponent.vue
@@ -12,12 +12,38 @@
 
         <div class="right-container">
             
-            <Splide :options="{ rewind: true }" aria-label="My Favorite Images">
+            <Splide id="News-splide" :options="{ rewinde: false, width: '100%', height: 'calc(90vh - 94px)' }" aria-label="My Favorite Images">
                 <SplideSlide>
-                <img src="/img/ios.png" alt="Sample 1">
+                
+                    <div class="splide-item-container" style="background-image: url('https://digitalhub.fifa.com/transform/4e4717fc-7f87-4ea4-b989-7f5730ec94ae/General-Graphic-3840-x-2160-8?&io=transform:fill,aspectratio:16x9&quality=75');">
+                        <div class="splide-item-text">
+                            <span class="subtitle">
+                                DESCUBRE
+                            </span>
+                            <h5>
+                                Planteles oficiales para la Copa Mundial Femenina Sub-17
+                            </h5>
+                            <p>
+                                Ya se conocen las listas de los equipos que competirán en República Dominicana. Choe Il Son, figura en el Sub-20 de Colombia, será el gran nombre.
+                            </p>
+                        </div>
+                    </div>
                 </SplideSlide>
                 <SplideSlide>
-                <img src="/img/play.png" alt="Sample 1">
+                
+                    <div class="splide-item-container" style="background-image: url('https://digitalhub.fifa.com/transform/3fac02c0-4860-4d98-9e6c-f242f8904f4c/Korea-DPR-v-Japan-Final-FIFA-U-20-Women-s-World-Cup-Colombia-2024?focuspoint=0.47,0.24&io=transform:fill,aspectratio:16x9&quality=75');">
+                        <div class="splide-item-text">
+                            <span class="subtitle">
+                                DESCUBRE
+                            </span>
+                            <h5>
+                                Planteles oficiales para la Copa Mundial Femenina Sub-17
+                            </h5>
+                            <p>
+                                Ya se conocen las listas de los equipos que competirán en República Dominicana. Choe Il Son, figura en el Sub-20 de Colombia, será el gran nombre.
+                            </p>
+                        </div>
+                    </div>
                 </SplideSlide>
             </Splide>
           
